d25c6a61d3c08bd68d9c73920cfac31c398e3128
Jean le Roux <jeanlrx@gmail.com>
2022-05-05 11:34:12 +1000

    remove api class

--- lib/zepto_client.rb
+++ lib/zepto_client.rb
@@ -12,57 +12,57 @@ require 'zepto_client/payment_request'
 require 'zepto_client/railtie' if defined?(Rails)
 
 module ZeptoClient
-  class Api
-    include Agreements
-    include ContactsReceivable
-    include Contacts
-    include PaymentRequests
-    include Payments
-    attr_accessor :api_key, :base_url
+  # class Api
+  #   include Agreements
+  #   include ContactsReceivable
+  #   include Contacts
+  #   include PaymentRequests
+  #   include Payments
+  #   attr_accessor :api_key, :base_url
 
-    def initialize(api_key:)
-      @api_key = api_key
-      @base_url = Rails.env.production? ?  "https://api.split.cash/" : "https://api.sandbox.split.cash/"
-    end
+  #   def initialize(api_key:)
+  #     @api_key = api_key
+  #     @base_url = Rails.env.production? ?  "https://api.split.cash/" : "https://api.sandbox.split.cash/"
+  #   end
 
-    private
+  #   private
 
-    def http
-      @http ||= HTTParty
-    end
+  #   def http
+  #     @http ||= HTTParty
+  #   end
 
-    def headers
-      @headers ||= { "Authorization": "Bearer #{@api_key}"}
-    end
+  #   def headers
+  #     @headers ||= { "Authorization": "Bearer #{@api_key}"}
+  #   end
 
-    def get(endpoint)
-      response = HTTParty.get(@base_url + endpoint, headers: headers)
-      response.parsed_response.deep_symbolize_keys
-    rescue
-      { errors: "Failed to connect to Zepto" }
-    end
+  #   def get(endpoint)
+  #     response = HTTParty.get(@base_url + endpoint, headers: headers)
+  #     response.parsed_response.deep_symbolize_keys
+  #   rescue
+  #     { errors: "Failed to connect to Zepto" }
+  #   end
 
-    def patch(endpoint, body)
-      response = HTTParty.patch(@base_url + endpoint, headers: headers, body: body)
-      response.parsed_response.deep_symbolize_keys
-    rescue
-      { errors: "Failed to connect to Zepto" }
-    end
+  #   def patch(endpoint, body)
+  #     response = HTTParty.patch(@base_url + endpoint, headers: headers, body: body)
+  #     response.parsed_response.deep_symbolize_keys
+  #   rescue
+  #     { errors: "Failed to connect to Zepto" }
+  #   end
 
-    def post(endpoint, body)
-      response = HTTParty.post(@base_url + endpoint, headers: headers, body: body)
-      response.parsed_response.deep_symbolize_keys
-    rescue
-      { errors: "Failed to connect to Zepto" }
-    end
+  #   def post(endpoint, body)
+  #     response = HTTParty.post(@base_url + endpoint, headers: headers, body: body)
+  #     response.parsed_response.deep_symbolize_keys
+  #   rescue
+  #     { errors: "Failed to connect to Zepto" }
+  #   end
 
-    def delete(endpoint, body)
-      response = HTTParty.post(@base_url + endpoint, headers: headers)
-      response.parsed_response.deep_symbolize_keys
-    rescue
-      { errors: "Failed to connect to Zepto" }
-    end
+  #   def delete(endpoint, body)
+  #     response = HTTParty.post(@base_url + endpoint, headers: headers)
+  #     response.parsed_response.deep_symbolize_keys
+  #   rescue
+  #     { errors: "Failed to connect to Zepto" }
+  #   end
 
-  end
+  # end
 
 end
