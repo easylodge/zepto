d7bb312fa43a7ca310e224f3ed22b7ee5e9a11c5
Jean le Roux <jeanlrx@gmail.com>
2022-05-05 12:07:12 +1000

    add more requires

--- lib/zepto_client.rb
+++ lib/zepto_client.rb
@@ -1,6 +1,8 @@
 require "httparty"
 require "active_record"
 
+require "zepto_client/api"
+require "zepto_client/payment_request"
 require 'zepto_client/railtie' if defined?(Rails)
 
 module ZeptoClient
