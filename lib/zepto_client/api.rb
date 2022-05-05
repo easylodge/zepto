438538c4cb797cbef6aabe82ae60dce3779ca9fe
Jean le Roux <jeanlrx@gmail.com>
2022-05-05 11:51:53 +1000

    stuuuuuf

--- lib/zepto_client/api.rb
+++ lib/zepto_client/api.rb
@@ -3,9 +3,7 @@ require "zepto_client/agreements"
 require "zepto_client/contacts_receivable"
 require "zepto_client/contacts"
 require "zepto_client/payment_requests"
-require "zepto_client/payments"
 require "zepto_client/unassigned_agreements"
-# require 'zepto_client/payment_request'
 
 class Api
   include Agreements
