4899c9252d2cb80a988711499452b2f3b04700aa
Jean le Roux <jeanlrx@gmail.com>
2022-05-05 11:01:59 +1000

    change order to test

--- lib/zepto_client.rb
+++ lib/zepto_client.rb
@@ -1,14 +1,13 @@
 require "httparty"
 require "active_record"
 
-require 'zepto_client/payment_request'
-
 require "zepto_client/agreements"
 require "zepto_client/contacts_receivable"
 require "zepto_client/contacts"
 require "zepto_client/payment_requests"
 require "zepto_client/payments"
 require "zepto_client/unassigned_agreements"
+require 'zepto_client/payment_request'
 
 require 'zepto_client/railtie' if defined?(Rails)
 
